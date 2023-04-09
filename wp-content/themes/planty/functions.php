3f7708393a45e2b8074b33ba13beea7fa6db9526
narimaneaibeche <aibechenarimane@gmail.com>
2023-04-09 12:01:40 +0200

    modification de lien Admin

--- wp-content/themes/planty/functions.php
+++ wp-content/themes/planty/functions.php
@@ -20,7 +20,7 @@ add_filter( 'wp_nav_menu_items', 'add_extra_item_to_nav_menu', 10, 2);
 function add_extra_item_to_nav_menu( $items, $args ) {
 
     if (is_user_logged_in() && $args->theme_location == 'main-menu' ) {
-        $items .= '<li ><a  href="http://localhost:8888/Planty2/wp-admin/">Admin</a></li>';
+        $items .= '<li ><a  href="/Planty2/wp-admin/">Admin</a></li>';
         
     }
     return $items;
